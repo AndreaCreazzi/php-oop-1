7a4e291787059d2bd8c9712821dd984537073a0d
Andrea Creazzi <andreacreazzi@libero.it>
2023-07-21 15:31:44 +0200

    add Class Movie (complete)

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
+<?php
+class Movie
+{
+    public $title;
+    public $language;
+    public $vote;
+    public $description;
 
+    function __construct($title, $language, $vote, $description)
+    {
+        $this->title = $title;
+        $this->language = $language;
+        $this->vote = $vote;
+        $this->description = $description;
+    }
+
+    public function getToUpperLanguage()
+    {
+        return  strtoupper($this->language);
+    }
+}
+
+$mummia = new Movie('mummia', 'ita', 3, 'lorem');
+
+var_dump($mummia);
+
+echo $mummia->getToUpperLanguage();
